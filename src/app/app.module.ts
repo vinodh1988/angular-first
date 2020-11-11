bf357f5ab830a5891f4fee936797481fffd685ac
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-11 10:03:08 +0530

    menu-block-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SecondComponent } from './second/second.component';
+import { MenuBlockComponent } from './menu-block/menu-block.component';
 // all your components need to be specified in declarations
 // all your pipes need to be specified in declarations
 // all your directives need to be specified in declerations
@@ -16,7 +17,8 @@ import { SecondComponent } from './second/second.component';
 @NgModule({
   declarations: [
     AppComponent,
-    SecondComponent
+    SecondComponent,
+    MenuBlockComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/menu-block/menu-block.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MenuBlockComponent } from './menu-block.component';
+
+describe('MenuBlockComponent', () => {
+  let component: MenuBlockComponent;
+  let fixture: ComponentFixture<MenuBlockComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MenuBlockComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MenuBlockComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
