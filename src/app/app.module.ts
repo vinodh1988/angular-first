5bc69334d5d31ae84273f8d35056986147941148
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-11 08:59:30 +0530

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// all your components need to be specified in declarations
+// all your pipes need to be specified in declarations
+// all your directives need to be specified in declerations
+
+//suppose if you are using external modules, you need to specify in imports
+// in providers we might specify services
+
+//every module will have one or more root components
 
 @NgModule({
   declarations: [
